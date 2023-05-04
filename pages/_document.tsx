a4c463ae10caf61c3bbbe1d170194b067b1dff47
Ivo Zayas <ivozayas2014@gmail.com>
2023-05-04 11:21:21 -0300

    title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title>cute foxes</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
